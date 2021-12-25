1d81d2d46db9ce755ccb66f6296161764a93fb8b
jibujacob <jibupjacob@gmail.com>
2021-12-25 19:29:21 +0530

    skaffold config for local

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,14 @@ app.use(express.json())
 
 const port = process.env.PORT || 5001;
 
+app.get('/',(req,res)=>{
+    res.send("Hello")
+})
+app.get("/api/users/currentUser",(req,res) =>{
+    console.log("Calling currentUser");
+    res.send("Current User")
+})
+
 app.listen(port,()=>{
-    console.log(`Auth Services listening on ${port}`);
+    console.log(`Auth Services listening on ${port}!!`);
 });
