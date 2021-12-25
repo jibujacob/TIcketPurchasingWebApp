c3366ae721df9b195cc04b80eeaec004d36446e9
jibujacob <jibupjacob@gmail.com>
2021-12-25 23:56:35 +0530

    Error handler implementation for Auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,19 +1,41 @@
 import express from "express";
+import "express-async-errors";
+import mongoose from "mongoose";
 
-const app = express()
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found";
 
-app.use(express.json())
+const app = express();
 
+app.use(express.json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*",async () => {
+    throw new NotFoundError();  
+})
+
+app.use(errorHandler)
 const port = process.env.PORT || 5001;
 
-app.get('/',(req,res)=>{
-    res.send("Hello")
-})
-app.get("/api/users/currentUser",(req,res) =>{
-    console.log("Calling currentUser");
-    res.send("Current User")
-})
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+        console.log("Connected to Auth Mongo DB");
+        app.listen(port,()=>{
+            console.log(`Auth Services listening on ${port}!!`);
+        });   
+    } catch (error) {
+        console.log(error);   
+    }
+}
 
-app.listen(port,()=>{
-    console.log(`Auth Services listening on ${port}!!`);
-});
+start();
+
