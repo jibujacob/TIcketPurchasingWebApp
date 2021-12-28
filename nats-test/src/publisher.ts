90d285987f1c5c59f4d7e397ac45cfc89ece532d
jibujacob <jibupjacob@gmail.com>
2021-12-28 23:10:36 +0530

    NATS Demo,Base Implementation and Push to common module and re-sync

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,21 @@
+import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
+
+console.clear();
+const stan = nats.connect('ticketing','abc',{
+    url:'http://localhost:4222',
+});
+
+stan.on('connect',async (err) => {
+    console.log("Publisher connected to NATS");
+    
+    const data = {id:"123",title:"concert",price:20};
+
+    const publisher = new TicketCreatedPublisher(stan)
+    try{
+        await publisher.publish(data);
+    }catch(err){
+        console.error(err);
+    }
+
+});
