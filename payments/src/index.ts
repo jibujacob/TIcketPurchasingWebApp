7d21d7c6e69b1b3e2a16383d257f22e76ee0484d
jibujacob <jibupjacob@gmail.com>
2022-01-03 22:09:11 +0530

    Starup message for payment added

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -8,6 +8,8 @@ import { natsWrapper } from "./nats-wrapper";
 const port = process.env.PORT || 5001;
 
 const start = async () => {
+    console.log("Starting...");
+    
     if(!process.env.JWT_KEY){
         throw new Error("JWT_KEY must be defined")
     }
