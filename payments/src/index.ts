1062f2d45474d30e87b6d3eaf815ac64c1637129
jibujacob <jibupjacob@gmail.com>
2022-01-04 09:59:25 +0530

    K8 setup test

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -8,7 +8,7 @@ import { natsWrapper } from "./nats-wrapper";
 const port = process.env.PORT || 5001;
 
 const start = async () => {
-    console.log("Starting payments service");
+    console.log("Starting up payments service...");
     
     if(!process.env.JWT_KEY){
         throw new Error("JWT_KEY must be defined")
