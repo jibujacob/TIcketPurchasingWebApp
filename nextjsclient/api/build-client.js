c592d5c6cfe75be52fd6364d764d3f20cfc072c5
jibujacob <jibupjacob@gmail.com>
2022-01-04 11:03:45 +0530

    DNS Setup

--- nextjsclient/api/build-client.js
+++ nextjsclient/api/build-client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const buildClient = ({req}) =>{
     if(typeof window === "undefined"){
         return axios.create({
-            baseURL : "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+            baseURL : "http://www.test-app-jpj-prod.xyz/",
             headers: req.headers,
         })
     }else{
